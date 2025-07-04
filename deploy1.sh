bd18432f8bcb0c02c0ce427ddd965a4e3bfaeb49
M Awais <ranag4u69@gmail.com>
2025-07-04 09:31:41 +0500

    Update deploy1.sh

--- deploy1.sh
+++ deploy1.sh
@@ -41,7 +41,7 @@ echo "Installing unzip"
 sudo apt-get install -y unzip
 
 echo "Install AWS CLI"
-curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o awscliv2.zip
+curl "https://awscli.amazonaws.com/awscli-exe-linux-aarch64.zip" -o "awscliv2.zip"
 unzip awscliv2.zip
 sudo ./aws/install
 
